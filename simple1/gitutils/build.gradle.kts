68763d2b0379fbbec05b004e2f8c73bcc3c81f33
Cedric Champeau <cedric.champeau@gmail.com>
2019-03-26 09:12:59 +0100

    Replace versions for SLF4J

--- simple1/gitutils/build.gradle.kts
+++ simple1/gitutils/build.gradle.kts
@@ -7,13 +7,12 @@ repositories {
 }
 
 dependencies {
-    implementation("org.slf4j:slf4j-api:1.7.26")
+    implementation("org.slf4j:slf4j-api:1.7.2")
     implementation("org.eclipse.jgit:org.eclipse.jgit:5.3.0.201903130848-r")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.4.1")
-    testRuntimeOnly("org.slf4j:slf4j-simple:1.7.26")
+    testRuntimeOnly("org.slf4j:slf4j-simple:1.7.2")
 }
-
 
 tasks.test {
     useJUnitPlatform()
